8d5923f16e2abc0e4cd926fc63f02df2aa4df80a
dhinesh <dhineshkaran6797@gmail.com>
2023-10-07 12:26:51 +0530

    check

---
+++ cmd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if the local repository is up to date with the remote
+git fetch
+
+# Compare the local branch with the remote branch
+DIFF=$(git rev-list HEAD...origin/aws --count)
+
+if [ $DIFF -gt 0 ]; then
+  echo "Changes found"
+else
+  echo "No changes found"
+fi
+
